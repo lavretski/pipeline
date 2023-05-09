80cf8db980e4d93d3ec7b432d0ac1f24cf5de946
viktor.lavretskyi.pm.2019 <viktor.lavretskyi.pm.2019@lpnu.ua>
2023-05-09 15:46:49 +0300

    0.482

--- store-sales-pipeline/fit_predict.py
+++ store-sales-pipeline/fit_predict.py
@@ -1,4 +1,3 @@
-import os
 import torch
 from losses import RMSELoss, RMSLELoss
 from models.Linears import DlinearModel, NlinearModel
@@ -7,30 +6,25 @@ from datasets import TimeSeriesDataset
 from loaders import TimeSeriesDataLoader
 from predicts import get_horizons
 from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau
-from early_stopping import EarlyStopping
-from pathes import path_to_temp
-from evaluate import evaluate_horizons
 
 
 def fit_predict(sales, device):
     seq_len = 150
-    lr = 0.001
+    lr = 0.0001
     epochs = 15
     feature_range = (0, 100)
     batch_size = 3
     factor = 0.5
     patience_scheduler = 3
-    patience_early_stopping = 10
-    best_model_path = os.path.join(path_to_temp, 'DLinear.pth')
 
     criterion = RMSELoss()
-    early_stopping = EarlyStopping(path=best_model_path, patience=patience_early_stopping, verbose=True)
     model = DlinearModel(seq_len=seq_len, pred_len=16).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     scheduler = ReduceLROnPlateau(optimizer, patience=patience_scheduler, factor=factor)
     scaler = MinMaxLogScaler(feature_range=feature_range)
     train_dataset = TimeSeriesDataset(data=sales[:-16], seq_len=seq_len, pred_len=16, scaler=scaler)
     train_loader = TimeSeriesDataLoader(train_dataset, batch_size=batch_size)
+
 
     for epoch in range(epochs):
         running_loss = 0.0
@@ -45,13 +39,4 @@ def fit_predict(sales, device):
         scheduler.step(running_loss / len(train_loader))
         print(f"Epoch {epoch + 1}/{epochs}, Loss: {running_loss / len(train_loader):.6f}")
 
-        criterion = RMSLELoss()
-        horizons = get_horizons(model, sales[-seq_len - 16:-16], scaler=scaler, device=device)
-        vali_loss = evaluate_horizons(sales[-16:], horizons, criterion)
-        early_stopping(vali_loss, model)
-        if early_stopping.early_stop:
-            print("Early stopping")
-            break
-
-    model.load_state_dict(torch.load(best_model_path))
     return get_horizons(model, sales[-seq_len:], scaler=scaler, device=device)
