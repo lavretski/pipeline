5b4858a6f842268b821a54ae143bfbfbb471acca
viktor.lavretskyi.pm.2019 <viktor.lavretskyi.pm.2019@lpnu.ua>
2023-05-20 19:33:22 +0300

    0.4323

--- store-sales-pipeline/models/Linears.py
+++ store-sales-pipeline/models/Linears.py
@@ -71,6 +71,15 @@ class DlinearModel(nn.Module):
         self.Linear_Seasonal = nn.Linear(self.seq_len, self.pred_len)
         self.Linear_Trend = nn.Linear(self.seq_len, self.pred_len)
 
+        self.weight_init = 1.0 / self.seq_len
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        nn.init.constant_(self.Linear_Seasonal.weight, self.weight_init)
+        nn.init.constant_(self.Linear_Seasonal.bias, 0)
+        nn.init.constant_(self.Linear_Trend.weight, self.weight_init)
+        nn.init.constant_(self.Linear_Trend.bias, 0)
+
     def forward(self, x):
         # x: [Batch, Input length, Channel]
         seasonal_init, trend_init = self.decompsition(x)
@@ -78,7 +87,55 @@ class DlinearModel(nn.Module):
         seasonal_output = self.Linear_Seasonal(seasonal_init)
         trend_output = self.Linear_Trend(trend_init)
         x = seasonal_output + trend_output
-        return torch.clip(x.permute(0, 2, 1), min=0)
+        return torch.abs(x.permute(0, 2, 1))
+
+
+class DlinearModelMulti(nn.Module):
+    """
+    Decomposition-Linear
+    """
+
+    def __init__(self, seq_len, hidden_size, pred_len):
+        super(DlinearModelMulti, self).__init__()
+        self.seq_len = seq_len
+        self.pred_len = pred_len
+        self.hidden_size = hidden_size
+
+        # Decompsition Kernel Size
+        kernel_size = 5
+        self.decompsition = series_decomp(kernel_size)
+
+        self.Linear_Seasonal1 = nn.Linear(self.seq_len, self.hidden_size)
+        self.Relu_Seasonal = nn.ReLU()
+        self.Linear_Seasonal2 = nn.Linear(hidden_size, self.pred_len)
+
+        self.Linear_Trend1 = nn.Linear(self.seq_len, self.hidden_size)
+        self.Relu_Trend = nn.ReLU()
+        self.Linear_Trend2 = nn.Linear(self.hidden_size, self.pred_len)
+
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        weight_init1 = 1.0 / self.seq_len
+        weight_init2 = 1.0 / self.hidden_size
+        nn.init.constant_(self.Linear_Seasonal1.weight, weight_init1)
+        nn.init.constant_(self.Linear_Seasonal1.bias, 0)
+        nn.init.constant_(self.Linear_Seasonal2.weight, weight_init2)
+        nn.init.constant_(self.Linear_Seasonal2.bias, 0)
+
+        nn.init.constant_(self.Linear_Trend1.weight, weight_init1)
+        nn.init.constant_(self.Linear_Trend1.bias, 0)
+        nn.init.constant_(self.Linear_Trend2.weight, weight_init2)
+        nn.init.constant_(self.Linear_Trend2.bias, 0)
+
+    def forward(self, x):
+        # x: [Batch, Input length, Channel]
+        seasonal_init, trend_init = self.decompsition(x)
+        seasonal_init, trend_init = seasonal_init.permute(0, 2, 1), trend_init.permute(0, 2, 1)
+        seasonal_output = self.Linear_Seasonal2(self.Relu_Seasonal(self.Linear_Seasonal1(seasonal_init)))
+        trend_output = self.Linear_Trend2(self.Relu_Trend(self.Linear_Trend1(trend_init)))
+        x = seasonal_output + trend_output
+        return torch.abs(x.permute(0, 2, 1))
 
 
 class NlinearModel(nn.Module):
--- store-sales-pipeline/submission.py
+++ store-sales-pipeline/submission.py
@@ -1,3 +1,4 @@
+import numpy as np
 from tqdm import tqdm
 import pandas as pd
 import os
@@ -10,11 +11,21 @@ def horizons_to_submission(horizons):
     pd_submission = pd.read_csv(os.path.join(path_to_data, 'test.csv'))
     pd_submission['sales'] = 0
     family_list = get_family_list()
+
+    df_train = pd.read_csv(os.path.join(path_to_data, 'train.csv'))
+    c = df_train.groupby(["store_nbr", "family"]).sales.sum().reset_index().sort_values(["family", "store_nbr"])
+    c = c[c.sales == 0]
+    zero_forecasting_store_family = [[item[0], item[1]] for item in c[['family', 'store_nbr']].values]
+
     i = 0
     for family in family_list:
         for store_nbr in range(1, 54 + 1):
-            pd_submission.loc[
-                (pd_submission.family == family) & (pd_submission.store_nbr == store_nbr), ['sales']] = horizons[:, i].flatten().numpy()
+            if [family, store_nbr] in zero_forecasting_store_family:
+                pd_submission.loc[
+                    (pd_submission.family == family) & (pd_submission.store_nbr == store_nbr), ['sales']] = np.zeros_like(horizons[:, i].flatten().numpy())
+            else:
+                pd_submission.loc[
+                    (pd_submission.family == family) & (pd_submission.store_nbr == store_nbr), ['sales']] = horizons[:, i].flatten().numpy()
             i += 1
     pd_submission.drop(['date', 'store_nbr', 'family', 'onpromotion'], axis=1, inplace=True)
     pd_submission.to_csv(os.path.join(path_to_temp, 'submission.csv'), index=False)
